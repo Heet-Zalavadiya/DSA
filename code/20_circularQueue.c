50263cd3a14a3e3e0546b411b1e23bdcfc320f7c
Heet-Zalavadiya <heetzalavadia@gmail.com>
2025-06-02 19:40:49 +0530

    DSA

--- code/20_circularQueue.c
+++ code/20_circularQueue.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct queue
+struct circularQueue
 {
     int size;
     int f;
@@ -9,16 +9,16 @@ struct queue
     int *arr;
 };
 
-int isFull(struct queue *q)
+int isFull(struct circularQueue *q)
 {
-    if (q->r == q->size - 1)
+    if ((q->r + 1) % q->size == q->f)
     {
         return 1;
     }
     return 0;
 }
 
-int isEmpty(struct queue *q)
+int isEmpty(struct circularQueue *q)
 {
     if (q->r == q->f)
     {
@@ -27,51 +27,56 @@ int isEmpty(struct queue *q)
     return 0;
 }
 
-void enqueue(struct queue *q, int val)
+void enqueue(struct circularQueue *q, int val)
 {
     if (isFull(q))
     {
-        printf("This Queue is full");
+        printf("This Queue is full\n");
     }
     else
     {
-        q->r++;
+        q->r = (q->r + 1) % q->size;
         q->arr[q->r] = val;
         printf("Enqueued element: %d\n", val);
     }
 }
 
-int dequeue(struct queue *q)
+int dequeue(struct circularQueue *q)
 {
     int a = -1;
     if (isEmpty(q))
     {
-        printf("This Queue is Empty");
+        printf("This Queue is Empty\n");
     }
     else
     {
-        q->f++;
+        q->f = (q->f + 1) % q->size;
         a = q->arr[q->f];
-        return a;
     }
+    return a;
 }
 
 int main()
 {
-    struct queue q;
-    q.size = 100;
-    q.f = q.r = -1;
+    struct circularQueue q;
+    q.size = 4;
+    q.f = q.r = 0;
     q.arr = (int *)malloc(q.size * sizeof(int));
 
     // Enqueue few elements
     enqueue(&q, 12);
     enqueue(&q, 15);
+    enqueue(&q, 1);
     printf("Dequeuing element %d\n", dequeue(&q));
     printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Dequeuing element %d\n", dequeue(&q));
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+    enqueue(&q, 45);
 
     if (isEmpty(&q))
     {
-        printf("Queue is empty\n");
+        printf("Queue is Empty\n");
     }
     if (isFull(&q))
     {
