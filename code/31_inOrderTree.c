f6d02979c67df9a5f0d7837bf1824cdc9d941d1c
Heet-Zalavadiya <heetzalavadia@gmail.com>
2025-06-10 22:32:07 +0530

    DSA

---
+++ code/31_inOrderTree.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *creatNode(int data)
+{
+    struct Node *n;                                 // Creating a node pointer
+    n = (struct Node *)malloc(sizeof(struct Node)); // Allocating memory in the heap
+    n->data = data;                                 // Selecting the data
+    n->left = NULL;                                 // Setting the left and right childern to NULL
+    n->right = NULL;                                // Setting the left and right childern to NULL
+
+    return n; // Finally returning the created node
+}
+
+void InOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        InOrder(root->left);
+        printf("%d ", root->data);
+        InOrder(root->right);
+    }
+}
+
+int main()
+{
+    struct Node *p = creatNode(4);
+    struct Node *p1 = creatNode(1);
+    struct Node *p2 = creatNode(6);
+    struct Node *p3 = creatNode(5);
+    struct Node *p4 = creatNode(2);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    InOrder(p);
+    return 0;
+}
