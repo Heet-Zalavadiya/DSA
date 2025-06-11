4670e60fd91c4fa743abcec246c0c68e36049b2d
Heet-Zalavadiya <heetzalavadia@gmail.com>
2025-06-11 22:05:12 +0530

    DSA

---
+++ code/32_binarySearchTree.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *creatNode(int data)
+{
+    struct Node *n;                                 // Creating a node pointer
+    n = (struct Node *)malloc(sizeof(struct Node)); // Allocating memory in the heap
+    n->data = data;                                 // Selecting the data
+    n->left = NULL;                                 // Setting the left and right childern to NULL
+    n->right = NULL;                                // Setting the left and right childern to NULL
+
+    return n; // Finally returning the created node
+}
+
+void InOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        InOrder(root->left);
+        printf("%d ", root->data);
+        InOrder(root->right);
+    }
+}
+
+int isBST(struct Node *root)
+{
+    static struct Node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    // Constructing the root node using function
+    struct Node *p = creatNode(5);
+    struct Node *p1 = creatNode(3);
+    struct Node *p2 = creatNode(6);
+    struct Node *p3 = creatNode(1);
+    struct Node *p4 = creatNode(4);
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    InOrder(p);
+    printf("\n");
+    if (isBST(p))
+    {
+        printf("The tree is a Binary Search Tree.\n");
+    }
+    else
+    {
+        printf("The tree is not a Binary Search Tree.\n");
+    }
+    return 0;
+}
