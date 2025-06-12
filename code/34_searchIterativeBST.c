ecfb582f19294b23e660951fe9c9299cc729fc0c
Heet-Zalavadiya <heetzalavadia@gmail.com>
2025-06-12 22:22:09 +0530

    DSA

---
+++ code/34_searchIterativeBST.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *creatNode(int data)
+{
+    struct Node *n;                                 // Creating a node pointer
+    n = (struct Node *)malloc(sizeof(struct Node)); // Allocating memory in the heap
+    n->data = data;                                 // Selecting the data
+    n->left = NULL;                                 // Setting the left and right childern to NULL
+    n->right = NULL;                                // Setting the left and right childern to NULL
+
+    return n; // Finally returning the created node
+}
+
+void InOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        InOrder(root->left);
+        printf("%d ", root->data);
+        InOrder(root->right);
+    }
+}
+
+int isBST(struct Node *root)
+{
+    static struct Node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+struct Node *searchIter(struct Node *root, int key)
+{
+    while (root != NULL)
+    {
+        if (key == root->data)
+        {
+            return root;
+        }
+        else if (key < root->data)
+        {
+            root = root->left;
+        }
+        else
+        {
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    // Constructing the root node using function
+    struct Node *p = creatNode(5);
+    struct Node *p1 = creatNode(3);
+    struct Node *p2 = creatNode(6);
+    struct Node *p3 = creatNode(1);
+    struct Node *p4 = creatNode(4);
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    struct Node *n = searchIter(p, 6);
+    if (n != NULL)
+    {
+        printf("Found : %d", n->data);
+    }
+    else
+    {
+        printf("Element Not Found ");
+    }
+
+    return 0;
+}
